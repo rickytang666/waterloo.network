565462a370db9168cb202ed3e9f5d5bdf6927b49
Zane Beeai <69683242+zanebeeai@users.noreply.github.com>
2026-01-20 23:08:27 -0500

    Update Zane Beeai's website URL
    new website!

--- src/data/members.ts
+++ src/data/members.ts
@@ -79,7 +79,7 @@ export const members: Member[] = [
   {
     id: "zane-beeai",
     name: "Zane Beeai",
-    website: "https://zane.beeai.com",
+    website: "https://zanebeeai.com/c/waterloo network",
     program: "Biomedical Engineering",
     year: "2030",
     profilePic: "/photos/zane-beeai.png",
